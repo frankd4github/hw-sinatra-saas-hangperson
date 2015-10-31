8d80faf9dc7e47c45648a0efc58a03a851ce6469
Frank Da Silva <frank.dasilva@gmail.com>
2015-10-31 18:13:55 +0000

    3 pending

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -16,6 +16,32 @@ class HangpersonGame
     @wrong_guesses = ''
   end
 
+  def guess(letter)
+    raise ArgumentError if letter.nil? or
+      letter.length != 1 or
+      letter !~ /[a-zA-Z]/
+    dletter = letter.downcase
+    return false if @guesses.include? dletter or @wrong_guesses.include? dletter
+    if @word.include? dletter
+      @guesses << dletter
+    else
+      @wrong_guesses << dletter
+    end
+    return true 
+  end
+  
+  def word_with_guesses
+    result = ''
+    @word.each_char do |letter|
+      if @guesses.include? letter
+        result << letter
+      else
+        result << '-'
+      end
+    end
+    return result
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
